7f3526d02cbb7e913e30c54223649cd173b92ca2
gil-cesar-martins <gilcesartst@gmail.com>
2022-04-03 19:14:02 -0300

    Correçao no wsgi.py

--- residenza/wsgi.py
+++ residenza/wsgi.py
@@ -3,6 +3,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling, MediaCling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'barpacoca.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'residenza.settings')
 
 application = Cling(MediaCling(get_wsgi_application()))
